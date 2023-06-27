047917b29066bb112203c132f3b336903f086784
Bitofsin <90807474+Bitofsin@users.noreply.github.com>
2023-06-26 21:26:55 -0700

    Create MoneroOcean-xmrig-compile.sh

---
+++ MoneroOcean-xmrig-compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Update and upgrade system
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Download and unzip xmrig
+wget https://github.com/MoneroOcean/xmrig/archive/refs/heads/master.zip
+unzip master.zip
+cd xmrig-master
+
+# Create build directory and compile xmrig
+mkdir build
+cd build
+cmake -DWITH_HWLOC=OFF ..
+make
+
+# Copy xmrig binary to home directory
+cp xmrig ~/xmrig
+
+# Create xmrigmoneroocean.sh in home directory
+echo '#!/bin/bash' > ~/xmrigmoneroocean.sh
+echo 'cd ~/xmrig' >> ~/xmrigmoneroocean.sh
+echo './xmrig -o gulf.moneroocean.stream:10032 -u 49EerUEecJxcFaJUGfEpyna91u5HU1ykHVJnP2BmnT4N3NpDd6qmdfPXzaFx6uRRzEiTMErpieeCiKwyhrbJ1JC29rtu66L -p x -t 3' >> ~/xmrigmoneroocean.sh
+
+# Make xmrigmoneroocean.sh executable
+chmod +x ~/xmrigmoneroocean.sh
