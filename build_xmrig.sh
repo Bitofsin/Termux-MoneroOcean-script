8bc74be7cecba775a20063f45840972f705689da
Bitofsin <90807474+Bitofsin@users.noreply.github.com>
2023-06-27 19:23:08 -0700

    Update build_xmrig.sh

--- build_xmrig.sh
+++ build_xmrig.sh
@@ -24,10 +24,28 @@ make
 # Copy xmrig binary to home directory
 cp xmrig ~/xmrig
 
+# Prompt for username
+read -p "Enter your username (Leave blank to keep default): " username
+if [[ -z $username ]]; then
+  username="49EerUEecJxcFaJUGfEpyna91u5HU1ykHVJnP2BmnT4N3NpDd6qmdfPXzaFx6uRRzEiTMErpieeCiKwyhrbJ1JC29rtu66L"
+fi
+
+# Prompt for password
+read -p "Enter your password (Leave blank to keep default): " password
+if [[ -z $password ]]; then
+  password="x"
+fi
+
+# Prompt for number of mining threads
+read -p "Enter the number of mining threads (Leave blank to keep default - 3): " mining_threads
+if [[ -z $mining_threads ]]; then
+  mining_threads="3"
+fi
+
 # Create xmrigmoneroocean.sh in home directory
 echo '#!/bin/bash' > ~/xmrigmoneroocean.sh
 echo 'cd ~/xmrig' >> ~/xmrigmoneroocean.sh
-echo './xmrig -o gulf.moneroocean.stream:10032 -u 49EerUEecJxcFaJUGfEpyna91u5HU1ykHVJnP2BmnT4N3NpDd6qmdfPXzaFx6uRRzEiTMErpieeCiKwyhrbJ1JC29rtu66L -p x -t 3 --randomx-mode light' >> ~/xmrigmoneroocean.sh
+echo "./xmrig -o gulf.moneroocean.stream:10032 -u $username -p $password -t $mining_threads --randomx-mode light" >> ~/xmrigmoneroocean.sh
 
 # Make xmrigmoneroocean.sh executable
 chmod +x ~/xmrigmoneroocean.sh
