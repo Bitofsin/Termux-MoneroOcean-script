c8e0ee4731468f811a0aa085ddb5102c78d38a45
Bitofsin <90807474+Bitofsin@users.noreply.github.com>
2023-06-27 19:39:47 -0700

    Update build_xmrig.sh

--- build_xmrig.sh
+++ build_xmrig.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Function to read user input with a timeout
+read_with_timeout() {
+    local prompt="$1"
+    local timeout="$2"
+    local response=""
+
+    # Read input in the background
+    read -p "$prompt" response &
+
+    # Start a timer in the background
+    sleep "$timeout" && kill -9 $$ 2>/dev/null &
+
+    # Wait for the input or the timeout
+    wait $! 2>/dev/null || :
+
+    # Return the user input or an empty string if timeout occurs
+    echo "$response"
+}
+
 # Update and upgrade system
 apt-get update
 apt-get upgrade -y
@@ -24,20 +43,20 @@ make
 # Copy xmrig binary to home directory
 cp xmrig ~/xmrig
 
-# Prompt for username
-read -p "Enter your username (Leave blank to keep default): " username
+# Prompt for username with timeout of 60 seconds
+username=$(read_with_timeout "Enter your username (Leave blank to keep default): " 60)
 if [[ -z $username ]]; then
   username="49EerUEecJxcFaJUGfEpyna91u5HU1ykHVJnP2BmnT4N3NpDd6qmdfPXzaFx6uRRzEiTMErpieeCiKwyhrbJ1JC29rtu66L"
 fi
 
-# Prompt for password
-read -p "Enter your password (Leave blank to keep default): " password
+# Prompt for password with timeout of 60 seconds
+password=$(read_with_timeout "Enter your password (Leave blank to keep default): " 60)
 if [[ -z $password ]]; then
   password="x"
 fi
 
-# Prompt for number of mining threads
-read -p "Enter the number of mining threads (Leave blank to keep default - 3): " mining_threads
+# Prompt for number of mining threads with timeout of 60 seconds
+mining_threads=$(read_with_timeout "Enter the number of mining threads (Leave blank to keep default - 3): " 60)
 if [[ -z $mining_threads ]]; then
   mining_threads="3"
 fi
